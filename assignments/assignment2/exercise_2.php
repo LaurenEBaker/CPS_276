e33cf906be1eab418db7e80f30566e138aa5315d
Lauren Baker <lebaker@russet-v8.wccnet.edu>
2023-09-29 17:08:30 -0400

    updates

---
+++ assignments/assignment2/exercise_2.php
@@ -0,0 +1,22 @@
+
+<!doctype html>
+<?php 
+$arr = array("string",43,true,$object,array(1,2,3)); 
+?>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    
+    <title>Form Project</title>
+  </head>
+  <body class= "container">
+    <h1> PHP Basics </h1>
+
+    <p> <?php echo $arr ?></p>
+    </body>
+    
+</html>

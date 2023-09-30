0f1c8e54a456fa9c7dad36de67bc0879a0cea2e4
Lauren Baker <lebaker@russet-v8.wccnet.edu>
2023-09-30 18:15:46 -0400

    updates

--- assignments/assignment2/exercise_2.php
+++ assignments/assignment2/exercise_2.php
@@ -1,5 +1,24 @@
-? php
-$arr = array("string",43,true,$object,array(1,2,3)); 
+$fruits = array (
+    "fruits"  => array("a" => "orange", "b" => "banana", "c" => "apple"),
+    "numbers" => array(1, 2, 3, 4, 5, 6),
+    "holes"   => array("first", 5 => "second", "third")
 
-print_r($arr);
-?>
+<!doctype html>
+<html lang="en">
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>fruit List</title>
+  </head>
+  <body>
+    <main class="container">
+      <h1>List of fruits</h1>
+    </main>  
+      <?php  
+       echo $fruits;   
+      ?>      
+  </body>
+</html>

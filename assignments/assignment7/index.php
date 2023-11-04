dab84cbddc690ac8f1012c6dd53afee873f77df1
Lauren Baker <lebaker@russet-v8.wccnet.edu>
2023-11-04 18:17:05 -0400

    updates

--- assignments/assignment7/index.php
+++ assignments/assignment7/index.php
@@ -12,6 +12,7 @@ $arr = $fileProc->init();
 	<title>File Upload and Display</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<link rel="stylesheet" href="public/css/main.css">
+  
   </head>
   <body>
   <form method="post" action="index.php"> 
